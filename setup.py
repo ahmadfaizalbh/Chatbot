e80955d693da3c58e629004337e160275be36158
Administrator <admin@example.com>
2017-09-18 11:59:03 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     url="https://github.com/ahmadfaizalbh/Chatbot",
     description="A chatbot AI engine is a chatbot builder platform that provids both bot intelligence and chat handler with minimal codding",
     long_description=open("README.rst").read(),
-    packages=['chatbot'],
+    packages=['chatbot','chatbot.spellcheck'],
     license='MIT',
     keywords='chatbot ai engine and chat builder platform',
     platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
-    package_dir={ 'chatbot': 'chatbot',},
+    package_dir={ 'chatbot': 'chatbot','chatbot.spellcheck':'chatbot/spellcheck'},
     include_package_data = True,
     package_data   = { "chatbot":  ["default.template","spellcheck/words.txt"]},
     install_requires=[
