90e05dd4b26ab1c282fe35cdf1e899078bd0bb45
Ahmad Faizal B H <ab4953@columbia.edu>
2020-08-03 17:04:56 +0530

    Add chatbot.substitution to setup.py packages

--- chatbot/version.py
+++ chatbot/version.py
@@ -1,4 +1,4 @@
 MAJOR = 0
 MINOR = 2
 MICRO = 2
-PATCH = 0
+PATCH = 1
--- setup.py
+++ setup.py
@@ -13,14 +13,15 @@ setup(
     description="A chatbot AI engine is a chatbot builder platform that provids both bot intelligence and"
                 " chat handler with minimal codding",
     long_description=open("README.rst").read(),
-    packages=['chatbot', 'chatbot.spellcheck'],
+    packages=['chatbot', 'chatbot.spellcheck', 'chatbot.substitution'],
     license='MIT',
     keywords='chatbot ai engine and chat builder platform',
     platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
-    package_dir={'chatbot': 'chatbot',
-                 'chatbot.spellcheck': 'chatbot/spellcheck',
-                 'chatbot.substitution': 'chatbot/substitution'
-                 },
+    package_dir={
+        'chatbot': 'chatbot',
+        'chatbot.spellcheck': 'chatbot/spellcheck',
+        'chatbot.substitution': 'chatbot/substitution'
+    },
     include_package_data=True,
     package_data={"chatbot":  ["local/en/default.template",
                                "local/en/words.txt",
