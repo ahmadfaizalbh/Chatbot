b601fbf5312e608fcc08f9a45f1a94930956e7da
mordi <mordi77787@gmail.com>
2021-03-02 16:20:12 +0200

    Add Hebrew language and support in encoding utf-8

--- chatbot/constansts.py
+++ chatbot/constansts.py
@@ -2,10 +2,12 @@ FIRST_QUESTIONS = {
     "en": "Hi, how are you?",
     "de": "Hallo, wie geht es dir?",
     "pt-br": "Oi como você está?",
+    "he": "היי מה קורה?",
 }
 TERMINATES = {
     "en": "quit",
     "de": "ende",
-    "pt-br": "sair"
+    "pt-br": "sair",
+    "he": "זהו"
 }
-LANGUAGE_SUPPORT = ["en", "de", "pt-br"]
+LANGUAGE_SUPPORT = ["en", "de", "pt-br","he"]
