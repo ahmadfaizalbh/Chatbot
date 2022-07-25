5ac7baa48fba60f3695973c54f159a3c6de7df89
Sourcery AI <>
2022-07-25 21:00:00 +0000

    'Refactored by Sourcery'

--- setup.py
+++ setup.py
@@ -14,11 +14,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 for language in LANGUAGE_SUPPORT:
-    package_data.extend([
-        "local/%s/default.template" % language,
-        "local/%s/words.txt" % language,
-        "local/%s/substitutions.json" % language
-    ])
+    package_data.extend(
+        [
+            f"local/{language}/default.template",
+            f"local/{language}/words.txt",
+            f"local/{language}/substitutions.json",
+        ]
+    )
+
 package_dir = {
         'chatbot': 'chatbot',
         'chatbot.spellcheck': 'chatbot/spellcheck',
